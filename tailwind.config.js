fa9671921df2c380f84956c6ae8fabe1497f38f4
Livio Alvarenga <livioalvarenga@gmail.com>
2022-11-09 11:59:28 -0300

    add versão desktop em tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,19 @@ module.exports = {
     content: ["./site/index.html"],
     theme: {
         extend: {
-            fontFamily: {
-                sans: "Inter, sans-serif",
+            backgroundImage: {
+                banner: "url(../../img/Background.png)",
             },
+            colors: {
+                brancoPrincipal: "#ffffff",
+                cinzaSecundario: "#C0C0C0",
+                botaoAzul: "#167BF7",
+                corDeFundo: "#00030C",
+                botaoAzulEfeito: "#3c92fa",
+            },
+        },
+        fontFamily: {
+            inter: ["Inter", "sans-serif"],
         },
     },
     plugins: [],
